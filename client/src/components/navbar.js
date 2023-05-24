f5c5bf1955466901b1f220449adcc10c2ab0385a
brandongiordano <bgiordano9917@aol.com>
2023-05-24 15:05:20 -0400

    creating login page

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,27 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 export const Navbar = () => {
-    return <div className="navbar">
+    const [cookies, setCookies] = useCookies(['access_token']);
+    const navigate = useNavigate();
+
+    const logout = () => {
+            setCookies("access_token", "");
+            window.localStorage.clear();
+            navigate("/auth");
+    };
+
+    return (<div className="navbar">
         <Link to="/">Home</Link>
         <Link to="/createRecipe">Create</Link>
         <Link to="/savedRecipes">My Recipes</Link>
         <Link to="/auth">Login/Signup</Link>
+        {!cookies.access_token ? (
+        <Link to="/auth">Login/Register</Link>
+      ) : (
+        <button onClick={logout}> Logout </button>
+      )}
     </div>
+    )
 }
